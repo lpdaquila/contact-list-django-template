12fe9f3cd800d1de50b0d74a3d0c89e95eab9aa0
lpdaquila <lpd.lucas@outlook.com>
2025-05-16 16:48:55 -0300

    contact_views and templates added

---
+++ contact/views/contact_views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
+from contact.models import Contact
+
+def index(request):
+    contacts = Contact.objects.filter(show=True).order_by('-id')[:30]
+    
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contacts'
+    }
+    
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+    
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    
+    if not search_value:
+        return redirect('contact:index')
+    
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) | # "OR" verification
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')[:30]
+        
+    # print(contacts.query)
+    
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search',
+        'search_value': search_value,
+    }
+    
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+    
+    
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    
+    contact_name = f'{single_contact.first_name} {single_contact.last_name}'
+    
+    context = {
+        'contact': single_contact,
+        'site_title': contact_name,
+    }
+    
+    return render(
+        request,
+        'contact/contact.html',
+        context
+    )
--- project/settings.py
+++ project/settings.py
@@ -10,17 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+import environ
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4!!$u9to2_6c+@yfu@@je_d=htlht!kok016o@ohdz$y8%1@ra'
+SECRET_KEY = env('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -130,3 +135,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from project.local_settings import *
+except:
+    pass
